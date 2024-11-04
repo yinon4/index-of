bca76321afbaee7c8e7f1263c68f76b4dbbfe1ff
ינוק <u9268782@bsmch.net>
2024-11-04 20:26:15 +0200

    --css

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,21 +3,14 @@
 # Source the functions file
 source "$(dirname "$0")/functions.sh"
 
-
 # Check if enough arguments are provided
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <input directory> <output directory> [--public <path to public directory>]"
+    echo "Usage: $0 <input directory> <output directory>"
     exit 1
 fi
 
 INPUT_DIR="${1%/}"
 OUTPUT_DIR="${2%/}"
-PUBLIC_DIR=""
-
-# Parse optional arguments
-if [ "$3" == "--public" ]; then
-    PUBLIC_DIR="$4"
-fi
 
 # Check if the input directory exists
 if [ ! -d "$INPUT_DIR" ]; then
@@ -34,13 +27,7 @@ generate_index "$INPUT_DIR"
 # Convert all markdown files to html files
 md_to_html "$INPUT_DIR" "$OUTPUT_DIR"
 
-# Copy public directory to the output directory
-if [ -n "$PUBLIC_DIR" ]; then
-    cp -r "$PUBLIC_DIR" "$OUTPUT_DIR"
-    echo "Copied public directory to the output directory"
-fi
-
-# Delete the index.md files
+# Delete all index.md files
 delete_indexes "$INPUT_DIR"
 
 echo "Build completed successfully."
